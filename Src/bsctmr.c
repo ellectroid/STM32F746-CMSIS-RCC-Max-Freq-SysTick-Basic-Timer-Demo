0da1df1195c647590ad80d73fdc3a998c0b079e9
Ilya <ilya.sheykhon@gmail.com>
2022-03-01 14:40:52 +0100

    cleared update event flag in timer initialization

--- Src/bsctmr.c
+++ Src/bsctmr.c
@@ -6,6 +6,7 @@ void basic_timer_setup(TIM_TypeDef *TMR) {
 	TMR->PSC = (108000U / 2U) - 1U; //APB1 is 54MHz, timer is 2x APB1 Freq, so now the timer is at 2kHz; 16-bit value!!! 65535 max!
 	TMR->ARR = 2000U; //2000 ticks at 2kHz is 1s
 	TMR->CNT = 0x00; //start value 0
+	TMR->SR &= ~TIM_SR_UIF; //make sure the update flag is cleared
 	TMR->DIER |= TIM_DIER_UIE; //update event interrupt enable
 
 	if (TMR == TIM6) {
